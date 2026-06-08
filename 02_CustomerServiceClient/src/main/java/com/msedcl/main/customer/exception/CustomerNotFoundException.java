8dd12c856f43de14979169f36e5e9a9799a99571
Vivek Gohil <59043694+vivek-gohil@users.noreply.github.com>
2026-06-08 17:05:10 +0530

    Delete 02_CustomerServiceClient directory

--- 02_CustomerServiceClient/src/main/java/com/msedcl/main/customer/exception/CustomerNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.msedcl.main.customer.exception;
-
-public class CustomerNotFoundException extends RuntimeException {
-	public CustomerNotFoundException(String message) {
-		super(message);
-	}
-}
